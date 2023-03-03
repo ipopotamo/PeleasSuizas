77e46d3327ceec8b876cd493d6820840f05cfff0
ipopotamo <ignaciobarbieri2003@gmail.com>
2023-03-03 20:30:07 -0300

    buffo a bb y hace el cambas y anda a llorar a la lloreria argentina campeon

--- PeleasSuizas/Assets/Scripts/CRonrdas.cs
+++ PeleasSuizas/Assets/Scripts/CRonrdas.cs
@@ -1,22 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI; // PARA USAR CANVAS
+using UnityEngine.UI; 
+using UnityEngine.SceneManagement;
 
 public class CRonrdas : MonoBehaviour
 {
     public GameObject ronda1win;
     public GameObject ronda1win2;
-    public static int ronda1;
-    public static int ronda2;
-  
+   
     [SerializeField] private GameObject controlador; 
 
-    public VidaPJ2 vidaUwU2;
+
+    static private float win1P1;
+    static private float win1P2;
+
     public VidaPJ1 vidaUwU1;
+    public VidaPJ2 vidaUwU2;
+  
 
     private void Start()
     {
+       
         ronda1win  = GameObject.FindGameObjectWithTag("Ronda1PJ1");
         ronda1win2 = GameObject.FindGameObjectWithTag("Ronda1PJ2");
 
@@ -26,21 +31,64 @@ public class CRonrdas : MonoBehaviour
         DontDestroyOnLoad(controlador);
     }
    
+   
+
     void Update()
     {
         if(vidaUwU1.vida <= 0)
-        {
-            ronda1++;
-            //ronda1win2.SetActive(true);
-            print("vegeta gana");
+        {        
+            win1P2++;     
+            if(win1P2 == 1f){
+               round2();
+            }     
+            if(win1P2 == 2f){
+               round2();
+            }    
+            if(win1P2 == 3f)
+            {
+                SceneManager.LoadScene("Ganador");
+            }  
         }
+
 
         if(vidaUwU2.vida <= 0)
         {
-            ronda2++;
-            //ronda1win.SetActive(true);
-            print("virgil gana");
+            win1P1++;
+            if(win1P1 == 1f){
+               round2();
+            } 
+            if(win1P1 == 2f){
+               round2();
+            }  
+            if(win1P1 == 3f){
+               SceneManager.LoadScene("Ganador");
+            }   
+        
         }
     
     }
+
+    public void round2()
+    {  
+        if(vidaUwU1.vida <= 0){
+         SceneManager.LoadScene("SampleScene");
+        
+         print(win1P2);
+         print("-----------");
+         print(win1P1);
+        }
+        if(vidaUwU2.vida <= 0){
+         SceneManager.LoadScene("SampleScene");
+         
+         print(win1P1);
+         print("-----------");
+         print(win1P2);
+         
+         
+        }
+        
+    }
+
+
+
 }
