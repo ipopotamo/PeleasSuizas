ac1f1528deeb007eb4da02eba6b89b2d670a7133
ipopotamo <ignaciobarbieri2003@gmail.com>
2023-03-20 14:22:12 -0300

    agrege pjtas a la lista

--- PeleasSuizas/Assets/Scripts/SelectorScript/InicioJugador1.cs
+++ PeleasSuizas/Assets/Scripts/SelectorScript/InicioJugador1.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class InicioJugador1 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
     void Start()
     {
         int indexJugador = PlayerPrefs.GetInt("JugadorIndex");
