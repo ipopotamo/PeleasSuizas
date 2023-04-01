21415e503f8c494be5858bbbd6083338535bee50
ipopotamo <ignaciobarbieri2003@gmail.com>
2023-03-31 21:10:47 -0300

    comit rapido

--- PeleasSuizas/Assets/Scripts/testeosNacho/MovimientoIA.cs
+++ PeleasSuizas/Assets/Scripts/testeosNacho/MovimientoIA.cs
@@ -8,32 +8,24 @@ public class MovimientoIA : MonoBehaviour
 
    
 
-    [SerializeField] public float movi;
-
-    [SerializeField] private float tiempoSiguienteAtaque;
-    [SerializeField] private float tiempoEntreAtaques;
+    [SerializeField] public float movi; 
     
     [SerializeField] private GameObject ControlEnemigo;
 
-    [SerializeField] private float dano;
-
-    private Transform objetivo;
-
-    private Rigidbody2D rb;
-
-    private Animator animacion;
+    private Transform   objetivo;
+    private Rigidbody2D rb;    
+    private Animator    animacion;
 
     private Vector2 movimiento;
     public  Vector3 Direccion;
 
     public LayerMask WhatIsPlayer;
 
-    public float checkradius; // Determina cuando empieza a perseguir al jugador
-    public float RadioAtaque; // Determina cuando ataca al jugador
+    public float checkradius; // Determina cuando empieza a perseguir al jugador    
 
     public  bool ShouldRotate; //Should Rotate significa "Debe rotar"
     private bool isInChaseRange; // pregunta si esta a rango para perseguir 
-    private bool isInAttackRange; // pregunta si tiene que atacar o no
+    private bool isInAttackRange;
 
     void Start()
     {
@@ -45,15 +37,10 @@ public class MovimientoIA : MonoBehaviour
     
     void Update()
     {
-        if (tiempoSiguienteAtaque > 0) 
-        {
-            tiempoSiguienteAtaque -= Time.deltaTime;
-        }
+        animacion.SetBool("caminar", isInChaseRange);
 
-        animacion.SetBool("Corriendo", isInChaseRange);
-
-        isInChaseRange = Physics2D.OverlapCircle(transform.position, checkradius, WhatIsPlayer); // Crea un circulo en representacion al radio de la vista    
-        isInAttackRange = Physics2D.OverlapCircle(transform.position, RadioAtaque, WhatIsPlayer);// Crea un circulo en representacion al radio de ataque    
+        isInChaseRange  = Physics2D.OverlapCircle(transform.position, checkradius, WhatIsPlayer); // Crea un circulo en representacion al radio de la vista    
+            
 
         Direccion = objetivo.position - transform.position;
         float amgulo = Mathf.Atan2(Direccion.x,Direccion.y) * Mathf.Rad2Deg; // En el tutorial usan "Atan", NO Atan2, pero con el 1 hay un error
@@ -61,14 +48,12 @@ public class MovimientoIA : MonoBehaviour
         Direccion.Normalize();
         movimiento = Direccion;
        
-                   
-        
+                           
         if (ShouldRotate)
         {
             animacion.SetFloat("X", Direccion.x);
             animacion.SetFloat("Y", Direccion.y);
         }
-
     }
 
     private void FixedUpdate()
@@ -76,34 +61,20 @@ public class MovimientoIA : MonoBehaviour
         if (isInChaseRange && !isInAttackRange) 
         {
             MoveCharacter(movimiento);
-         //   animacion.SetBool("Ataque", isInAttackRange);
-        }
-
+        }        
     }
+
+
+
     private void MoveCharacter(Vector2 Direccion)
     {
         rb.MovePosition((Vector2)transform.position + (Direccion * movi * Time.deltaTime));
     }
 
 
-    private void Ataque()
-    {
-        Collider2D[] objetos = Physics2D.OverlapCircleAll(ControlEnemigo.transform.position, RadioAtaque);
-
-        foreach (Collider2D colisionador in objetos)
-        {
-            if (colisionador.CompareTag("Jugador1"))
-            {
-                colisionador.transform.GetComponent<VidaPJ1>().TomarDaño1(dano);
-            }
-        }
-    }
-
+  
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.blue;
-        Gizmos.DrawWireSphere(ControlEnemigo.transform.position, RadioAtaque);
-
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(ControlEnemigo.transform.position, checkradius);
     }
