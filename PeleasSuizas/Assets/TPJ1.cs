d5a011cc0aafc8d8e257388acf0405cfa5fef6eb
Octavio Lucardi Fierro <octaviolf2016@gmail.com>
2023-04-23 22:53:31 -0300

    Los TP son limitados, solo te podes Tpear si te tiran una habilidad, se regeneran cada 20s pero no se pueden ver en pantalla, agregar texto para eso

--- PeleasSuizas/Assets/TPJ1.cs
+++ PeleasSuizas/Assets/TPJ1.cs
@@ -4,21 +4,61 @@ using UnityEngine;
 
 public class TPJ1 : MonoBehaviour
 {
+    [SerializeField] private Transform areaTP;
+    private float radioTP = 4f;
+    [SerializeField] private float maxTP = 4f;
+    private float cantTP;
+    private float regeneracionTP = 20f;
+    [SerializeField] private float tiempoRTP;
+
+    private float tiempoEntreTP = 3;
+    private float tiempoSiguienteTP;
     private Transform TPaca;
     // Start is called before the first frame update
     void Start()
     {
+        cantTP = maxTP;
         TPaca = GameObject.FindGameObjectWithTag("TPPJ2").GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        TPalPJ2();   
-    }
-    public void TPalPJ2(){
-        if(Input.GetKey("q")){
-            this.transform.position = TPaca.transform.position;
+        if (tiempoSiguienteTP>0)
+        {
+            tiempoSiguienteTP -= Time.deltaTime;
         }
+        if(cantTP < maxTP){
+            tiempoRTP -= Time.deltaTime;
+        }
+        if(tiempoRTP < 0){
+            cantTP ++;
+            tiempoRTP = regeneracionTP;
+        }
+
+         TPalPJ2();
+
+    }
+    
+    public void TPalPJ2(){
+        if (Input.GetKey("q")){
+            Collider2D[] objetos = Physics2D.OverlapCircleAll(areaTP.position, radioTP);
+
+            foreach (Collider2D colisionador in objetos) {
+                if (colisionador.CompareTag("Habilidad")) 
+                {
+                    if(cantTP > 0 && tiempoSiguienteTP <= 0 ){
+                        tiempoSiguienteTP = tiempoEntreTP;
+                        this.transform.position = TPaca.transform.position;
+                        cantTP = cantTP - 1;
+                    }
+                }
+            }
+        }
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(areaTP.position, radioTP);
     }
 }
